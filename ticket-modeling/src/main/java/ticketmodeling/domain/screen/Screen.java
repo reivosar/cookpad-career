6649f48fa52792c35e332b17cb1a2084706ed12c
reivosar <reivosar@yahoo.co.jp>
2020-10-18 15:02:37 +0900

    refactoring

--- ticket-modeling/src/main/java/ticketmodeling/domain/screen/Screen.java
+++ ticket-modeling/src/main/java/ticketmodeling/domain/screen/Screen.java
@@ -1,14 +1,23 @@
 package ticketmodeling.domain.screen;
 
+import java.util.Collection;
+
+import ticketmodeling.domain.schedule.ScheduleId;
 import ticketmodeling.domain.shared.Entity;
 
 public class Screen extends Entity<ScreenId, Screen>
 {
 	final ScreenId screenId;
 	final ScreenProfile screenProfile;
+	final Collection<ScheduleId> schedules;
 
-	public Screen(ScreenId screenId, ScreenProfile screenProfile) {
+	public Screen(
+		ScreenId screenId,
+		ScreenProfile screenProfile,
+		Collection<ScheduleId> schedules)
+	{
 		this.screenId      = screenId;
 		this.screenProfile = screenProfile;
+		this.schedules     = schedules;
 	}
 }
--- ticket-modeling/src/main/java/ticketmodeling/domain/screen/ScreenName.java
+++ ticket-modeling/src/main/java/ticketmodeling/domain/screen/ScreenName.java
@@ -4,5 +4,9 @@ import ticketmodeling.domain.shared.ValueObject;
 
 public class ScreenName extends ValueObject<ScreenName>
 {
+	final String value;
 
+	public ScreenName(String value) {
+		this.value = value;
+	}
 }
