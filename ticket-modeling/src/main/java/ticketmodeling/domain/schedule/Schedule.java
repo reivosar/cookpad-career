ecb54b3beca6f22ea883908acb8271eecadd397c
reivosar <reivosar@yahoo.co.jp>
2020-10-20 04:13:48 +0900

    リファクタリング

--- ticket-modeling/src/main/java/ticketmodeling/domain/schedule/Schedule.java
+++ ticket-modeling/src/main/java/ticketmodeling/domain/schedule/Schedule.java
@@ -2,20 +2,24 @@ package ticketmodeling.domain.schedule;
 
 import ticketmodeling.common.domain.shared.Entity;
 import ticketmodeling.domain.movie.MovieId;
+import ticketmodeling.domain.screen.ScreenId;
 
 public class Schedule extends Entity<ScheduleId, Schedule>
 {
 	final ScheduleId scheduleId;
 	final ScheduledTime scheduledTime;
+	final ScreenId screenId;
 	final MovieId movieId;
 
 	public Schedule(
 		ScheduleId scheduleId,
 		ScheduledTime scheduledTime,
+		ScreenId screenId,
 		MovieId movieId)
 	{
 		this.scheduleId    = scheduleId;
 		this.scheduledTime = scheduledTime;
+		this.screenId      = screenId;
 		this.movieId       = movieId;
 	}
 }
--- ticket-modeling/src/main/java/ticketmodeling/domain/screen/Screen.java
+++ ticket-modeling/src/main/java/ticketmodeling/domain/screen/Screen.java
@@ -4,22 +4,26 @@ import java.util.Collection;
 
 import ticketmodeling.common.domain.shared.Entity;
 import ticketmodeling.domain.schedule.ScheduleId;
+import ticketmodeling.domain.theater.TheaterId;
 
 public class Screen extends Entity<ScreenId, Screen>
 {
 	final ScreenId screenId;
 	final ScreenProfile screenProfile;
+	final TheaterId theaterId;
 	final Seats seats;
 	final Collection<ScheduleId> schedules;
 
 	public Screen(
 		ScreenId screenId,
 		ScreenProfile screenProfile,
+		TheaterId theaterId,
 		Seats seats,
 		Collection<ScheduleId> schedules)
 	{
 		this.screenId      = screenId;
 		this.screenProfile = screenProfile;
+		this.theaterId     = theaterId;
 		this.seats         = seats;
 		this.schedules     = schedules;
 	}
