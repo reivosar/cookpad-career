29b11d57089c836215cba9694ae2bf07c0251c22
reivosar <reivosar@yahoo.co.jp>
2020-10-20 05:36:16 +0900

    リファクタリング

--- ticket-modeling/src/main/java/ticketmodeling/common/domain/shared/primitive/Money.java
+++ ticket-modeling/src/main/java/ticketmodeling/common/domain/shared/primitive/Money.java
@@ -10,6 +10,11 @@ public class Money extends ValueObject<Money>
 	final BigDecimal decimal;
 	final Currency currency;
 
+	public Money(int intValue, Currency currency) {
+		this.decimal  = BigDecimal.valueOf(intValue);
+		this.currency = currency;
+	}
+
 	public Money(BigDecimal decimal, Currency currency) {
 		this.decimal  = decimal;
 		this.currency = currency;
