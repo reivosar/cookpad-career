8197b36cd75374ec5a93080383d3005e30b8d833
reivosar <reivosar@yahoo.co.jp>
2020-10-22 05:29:10 +0900

    リファクタリング

--- ticket-modeling/src/main/java/ticketmodeling/domain/model/price/DateAndTimePriceType.java
+++ ticket-modeling/src/main/java/ticketmodeling/domain/model/price/DateAndTimePriceType.java
@@ -11,17 +11,20 @@ public enum DateAndTimePriceType
 	HOLIDAY_LATE_PRICE,
 	MOVIEDAY_PRICE;
 
-	public static DateAndTimePriceType of (YearMonthDay ymd, HourMinute hm) {
+	public static DateAndTimePriceType of (YearMonthDay ymd, HourMinute hm)
+	{
 		if (ymd.equalsDay(1))
 			return MOVIEDAY_PRICE;
 
 		if (ymd.isWeekend())
-			return hm.isAfter(new HourMinute(20, 00)) ?
-					HOLIDAY_LATE_PRICE :
-					HOLIDAY_NORMAL_PRICE;
+			return isLateHourMinute(hm) ? HOLIDAY_LATE_PRICE
+										: HOLIDAY_NORMAL_PRICE;
 
-		return hm.isAfter(new HourMinute(20, 00)) ?
-				WEEKDAY_LATE_PRICE:
-				WEEKDAY_NORMAL_PRICE;
+		return isLateHourMinute(hm) ? WEEKDAY_LATE_PRICE
+									: WEEKDAY_NORMAL_PRICE;
+	}
+
+	private static boolean isLateHourMinute(HourMinute hm) {
+		return hm.isAfter(new HourMinute(20, 00));
 	}
 }
