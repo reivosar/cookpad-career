38d394444fc77c9705fe0dd4719610b5f20cbf06
reivosar <nextcareerplus@gmail.com>
2021-03-28 22:44:46 +0900

    final修飾子付与

--- ticket-modeling/src/main/java/ticketmodeling/domain/model/audience/Audience.java
+++ ticket-modeling/src/main/java/ticketmodeling/domain/model/audience/Audience.java
@@ -19,21 +19,21 @@ public abstract class Audience<ENTITY extends Audience<ENTITY>>
 		this.id = id;
 	}
 
-	public Price calcScreenPrice(final YearMonthDay ymd, final HourMinute hm) {
+	public final Price calcScreenPrice(final YearMonthDay ymd, final HourMinute hm) {
 		DateAndTimePriceSpecific specific = new DateAndTimePriceSpecific(
 				type().priceTable(), ymd, hm);
 		return specific.calcPrice();
 	}
 
-	public Price calcScreenPrice(final ScheduledTime scheduledTime) {
+	public final Price calcScreenPrice(final ScheduledTime scheduledTime) {
 		return calcScreenPrice(scheduledTime.yearMonthDay(), scheduledTime.hourMinute());
 	}
 
-	public Ticket selectTicketToBuy(final Schedule schedule) {
+	public final Ticket selectTicketToBuy(final Schedule schedule) {
 		return new Ticket(id, schedule.publicId(), calcScreenPrice(schedule.time()));
 	}
 
-	public Ticket buyTicket(final Schedule schedule, final Money money) {
+	public final Ticket buyTicket(final Schedule schedule, final Money money) {
 		return selectTicketToBuy(schedule).buyTicket(money);
 	}
 
